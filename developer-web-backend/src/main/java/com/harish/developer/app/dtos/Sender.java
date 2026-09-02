6800f6ca095c4bad22ef1ecfb18a61d2e5359d69
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-02 23:46:05 +0530

    Add Sender and SenderModel DTOs for email handling

---
+++ developer-web-backend/src/main/java/com/harish/developer/app/dtos/Sender.java
@@ -0,0 +1,21 @@
+package com.harish.developer.app.dtos;
+
+public class Sender
+{
+	private String name;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
