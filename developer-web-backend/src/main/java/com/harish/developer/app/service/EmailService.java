708209e449a2a78f918c4b84bd2e423b0db02ef8
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-02 23:45:44 +0530

    Add pom.xml and EmailService for email sending functionality

---
+++ developer-web-backend/src/main/java/com/harish/developer/app/service/EmailService.java
@@ -0,0 +1,48 @@
+package com.harish.developer.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import com.harish.developer.app.dtos.Recipient;
+import com.harish.developer.app.dtos.ReplyTo;
+import com.harish.developer.app.dtos.Sender;
+import com.harish.developer.app.models.SenderModel;
+
+@Service
+public class EmailService
+{
+	@Autowired
+	private RestClient restClient;
+	
+	public String sendEmail(String name, String email, String message)
+	{
+		Sender sender = new Sender();
+		sender.setName("Harish-Portfolio");
+		sender.setEmail("harishss.2k07@gmail.com");
+		
+		Recipient recipient = new Recipient();
+		recipient.setName("Harish");
+		recipient.setEmail("harishss.2k07@gmail.com");
+		
+		ReplyTo replyTo = new ReplyTo();
+		replyTo.setName(name);
+		replyTo.setEmail(email);
+		
+		SenderModel senderModel = new SenderModel();
+		senderModel.setSender(sender);
+		senderModel.setTo(java.util.Arrays.asList(recipient));
+		senderModel.setReplyTo(replyTo);
+		senderModel.setSubject("New Contact Form Submission");
+		senderModel.setTextContent("Name: " + name + "\nEmail: " + email + "\nMessage: " + message);
+		
+		String response= restClient.post()
+								   .uri("/smtp/email")
+								   .body(senderModel)
+								   .retrieve()
+								   .body(String.class);
+		
+		System.out.println("Response from Brevo API: " + response);
+		return "Email sent successfully";
+	}
+
+}
