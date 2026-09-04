c18944495d2e1a02d79c2835fc8d4fe4ecb7bf66
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-05 00:02:10 +0530

    Add CrossOrigin annotation to EmailController for CORS support

--- developer-web-backend/src/main/java/com/harish/developer/app/v1/controller/EmailController.java
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/controller/EmailController.java
@@ -2,6 +2,7 @@ package com.harish.developer.app.v1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class EmailController
 	private EmailService emailService;
 	
 	@PostMapping("/send")
+	@CrossOrigin(origins = "http://localhost:5173")
 	public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest)
 	{
 		emailService.sendEmail(emailRequest.getName(), emailRequest.getEmail(), emailRequest.getMessage());
