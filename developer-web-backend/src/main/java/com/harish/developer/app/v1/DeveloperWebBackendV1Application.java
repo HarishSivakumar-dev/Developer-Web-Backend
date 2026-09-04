f42c052cd6c874bd2997fec8351c0a54a9c6c725
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-04 23:02:36 +0530

    initialize Developer Web Backend V1 application

---
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/DeveloperWebBackendV1Application.java
@@ -0,0 +1,13 @@
+package com.harish.developer.app.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperWebBackendV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperWebBackendV1Application.class, args);
+	}
+
+}
