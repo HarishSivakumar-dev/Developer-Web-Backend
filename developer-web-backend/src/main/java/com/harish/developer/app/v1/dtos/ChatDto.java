a16bba41b6a21c8922471acd7573fbb071b3adb2
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-05 20:39:10 +0530

    Add ChatDto class for chat message data transfer

---
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/dtos/ChatDto.java
@@ -0,0 +1,15 @@
+package com.harish.developer.app.v1.dtos;
+
+public class ChatDto
+{
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
