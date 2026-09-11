af6f8ad087b51525f368f0a09294e7d274f3f756
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-11 23:15:30 +0530

    Refactor EmbeddingService to use VectorStore and add method for loading embeddings

--- developer-web-backend/src/main/java/com/harish/developer/app/v1/service/EmbeddingService.java
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/service/EmbeddingService.java
@@ -2,28 +2,24 @@ package com.harish.developer.app.v1.service;
 
 import java.util.List;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
-
 import com.knuddels.jtokkit.api.EncodingType;
 
 @Component
 public class EmbeddingService 
 {
-	@Autowired
-	private EmbeddingModel model;
 	
-	public float[] getEmbedding(String input) 
+	private VectorStore vectorStore;
+	
+	public EmbeddingService(VectorStore vectorStore) 
 	{
-		// Call the embedding model to get the embedding for the input text
-		float[] embedding = model.embed(input);
-		return embedding;
+		this.vectorStore = vectorStore;
 	}
-	
-	public List<String> chunkTexts()
+
+	public List<Document> chunkTexts()
 	{
 		TextReader textReader = new TextReader("classpath:static/texts.txt");
 		List<Document> ls= textReader.get();
@@ -34,10 +30,14 @@ public class EmbeddingService
 					                                  .withEncodingType(EncodingType.O200K_BASE)
 					                                  .build();
 		List<Document> str= splitter.split(ls);
-		List<String> chunkedTexts = str.stream()
-		                             .map(Document::getText)
-		                             .toList();
-		return chunkedTexts;
+		return str;
+	}
+	public String loadVectorDbWithEmbeddings() 
+	{
+		List<Document> docs= chunkTexts();
+		vectorStore.add(docs);
+		
+		return "done";
 	}
 	
 }
