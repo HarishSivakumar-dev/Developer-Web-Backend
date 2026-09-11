17d1014a380a63a51b9700b65f2a55b14cbd2f61
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-11 16:05:47 +0530

    Add chunkTexts method to EmbeddingService for text processing

--- developer-web-backend/src/main/java/com/harish/developer/app/v1/service/EmbeddingService.java
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/service/EmbeddingService.java
@@ -1,8 +1,14 @@
 package com.harish.developer.app.v1.service;
 
+import java.util.List;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.knuddels.jtokkit.api.EncodingType;
 
 @Component
 public class EmbeddingService 
@@ -15,6 +21,23 @@ public class EmbeddingService
 		// Call the embedding model to get the embedding for the input text
 		float[] embedding = model.embed(input);
 		return embedding;
+	}
+	
+	public List<String> chunkTexts()
+	{
+		TextReader textReader = new TextReader("classpath:static/texts.txt");
+		List<Document> ls= textReader.get();
+		
+		TokenTextSplitter splitter = TokenTextSplitter.builder()
+					                                  .withChunkSize(100)
+					                                  .withMaxNumChunks(20)
+					                                  .withEncodingType(EncodingType.O200K_BASE)
+					                                  .build();
+		List<Document> str= splitter.split(ls);
+		List<String> chunkedTexts = str.stream()
+		                             .map(Document::getText)
+		                             .toList();
+		return chunkedTexts;
 	}
 	
 }
