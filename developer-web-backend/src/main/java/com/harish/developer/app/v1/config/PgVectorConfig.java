fbfff080aaab523a861e858fccd6e918e671fdcc
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-11 23:15:41 +0530

    Add PgVectorConfig for configuring PgVectorStore with JdbcTemplate

---
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/config/PgVectorConfig.java
@@ -0,0 +1,23 @@
+package com.harish.developer.app.v1.config;
+
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class PgVectorConfig
+{
+	@Bean
+	public VectorStore vectorStore(EmbeddingModel model, JdbcTemplate jdbcTemplate)
+	{
+		PgVectorStore config = PgVectorStore.builder(jdbcTemplate, model)
+										  	.vectorTableName("embeddings")
+										  	.build();	
+		
+		return config;
+	}
+
+}
