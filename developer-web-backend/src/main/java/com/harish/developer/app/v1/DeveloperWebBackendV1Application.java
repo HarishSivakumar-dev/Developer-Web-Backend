759372902e111cfa8de9aaed5e55be9054b8ab11
Harish Sivakumar <harishss.2k07@gmail.com>
2026-09-12 22:22:30 +0530

    Exclude GoogleGenAiChatAutoConfiguration from Spring Boot application

--- developer-web-backend/src/main/java/com/harish/developer/app/v1/DeveloperWebBackendV1Application.java
+++ developer-web-backend/src/main/java/com/harish/developer/app/v1/DeveloperWebBackendV1Application.java
@@ -1,9 +1,12 @@
 package com.harish.developer.app.v1;
 
+import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+	    exclude = GoogleGenAiChatAutoConfiguration.class
+	)
 public class DeveloperWebBackendV1Application {
 
 	public static void main(String[] args) {
